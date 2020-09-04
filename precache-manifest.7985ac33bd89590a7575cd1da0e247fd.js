1d706c24488d6381dd9d34627974f6c5449cbb30
Connie <connie70308@gmail.com>
2020-09-04 16:28:07 -0700

    Updates

---
+++ precache-manifest.7985ac33bd89590a7575cd1da0e247fd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b407e339c5bedd7a1aa3bc3d70c8003",
+    "url": "/Fletter-Frontend/index.html"
+  },
+  {
+    "revision": "4742edc3850a1dc3c629",
+    "url": "/Fletter-Frontend/static/css/main.0cf09d3c.chunk.css"
+  },
+  {
+    "revision": "91d1c66f0c77f71d42cb",
+    "url": "/Fletter-Frontend/static/js/2.3eca5cd1.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Fletter-Frontend/static/js/2.3eca5cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4742edc3850a1dc3c629",
+    "url": "/Fletter-Frontend/static/js/main.6ddf2ed3.chunk.js"
+  },
+  {
+    "revision": "8a87e0169b5682131233",
+    "url": "/Fletter-Frontend/static/js/runtime-main.e1acb3f2.js"
+  },
+  {
+    "revision": "7652e3a2223bbabee2e9b52ec66b5545",
+    "url": "/Fletter-Frontend/static/media/Logo.7652e3a2.png"
+  },
+  {
+    "revision": "ac4b1ebb1aa1f666558deee1d0f7ddc7",
+    "url": "/Fletter-Frontend/static/media/birdPost2.ac4b1ebb.png"
+  },
+  {
+    "revision": "c1c52507e58e96b8f57c8235b4387156",
+    "url": "/Fletter-Frontend/static/media/earth.c1c52507.png"
+  },
+  {
+    "revision": "202d3a8e83033566b289cf5d3f9ec21c",
+    "url": "/Fletter-Frontend/static/media/mailbox.202d3a8e.png"
+  },
+  {
+    "revision": "8a9cfb0a6b3739a01832c9e5b517ae3c",
+    "url": "/Fletter-Frontend/static/media/message.8a9cfb0a.png"
+  },
+  {
+    "revision": "1fdf5b40f4c8e61bfb36dae6c9422303",
+    "url": "/Fletter-Frontend/static/media/postbirdie.1fdf5b40.png"
+  }
+]);
